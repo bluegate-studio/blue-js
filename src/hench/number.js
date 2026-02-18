773baa3bcc865ee65c1559bc3f0075435ddc0eff
Bluegate Studio <144131259+bluegate-studio@users.noreply.github.com>
2026-02-18 21:11:52 +0300

    re-enable nearest_in_array with vanilla implementation; reformat number.js

--- src/hench/number.js
+++ src/hench/number.js
@@ -1,81 +1,93 @@
 import * as kp from '../_.js';
 
-export function int( input ) {
-	return valid( input, 'int' ); }
+export function int(input) {
+    return valid(input, 'int');
+}
 
-export function float( input ) {
-	return valid( input, 'float' ); }
+export function float(input) {
+    return valid(input, 'float');
+}
 
-export function valid( input, type ) {
+export function valid(input, type) {
 
-	if ( 'float' !== type ) {
-		type = 'int'; }
+    if ('float' !== type) {
+        type = 'int';
+    }
 
-	let output = 0;
-	try {
-		if ( 'float' === type ) {
-			output = parseFloat( input ); }
-		else {
-			output = parseInt( input ); }
-	} catch( err ) { output = 0; }
+    let output = 0;
+    try {
+        if ('float' === type) {
+            output = parseFloat(input);
+        }
+        else {
+            output = parseInt(input);
+        }
+    } catch (err) { output = 0; }
 
-	if ( isFinite( output ) && !isNaN( output ) ) { 
-		return output; }
+    if (isFinite(output) && !isNaN(output)) {
+        return output;
+    }
 
-	return 0;
+    return 0;
 
 }
 
 
-export function fathom( input, type ) {
+export function fathom(input, type) {
 
-	if ( 'float' !== type ) {
-		type = 'int'; }
+    if ('float' !== type) {
+        type = 'int';
+    }
 
-	let output = null;
-	try {
-		if ( 'float' === type ) {
-			output = parseFloat( input ); }
-		else {
-			output = parseInt( input ); }
-	} catch( err ) { output = null; }
+    let output = null;
+    try {
+        if ('float' === type) {
+            output = parseFloat(input);
+        }
+        else {
+            output = parseInt(input);
+        }
+    } catch (err) { output = null; }
 
-	if ( isFinite( output ) && !isNaN( output ) ) { 
-		return true; }
+    if (isFinite(output) && !isNaN(output)) {
+        return true;
+    }
 
-	return false;
+    return false;
 
 }
 
 
-export function random( min, max ) {
+export function random(min, max) {
 
-	min = Math.ceil( min );
-	max = Math.floor( max );
+    min = Math.ceil(min);
+    max = Math.floor(max);
 
-	return ( Math.floor( Math.random() * ( max - min + 1 ) ) + min );
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
 
 }
 
 export function nearest_in_array({ needle, haystack }) {
 
-    return false;
+    needle = float(needle);
+    haystack = kp.hench.array.valid(haystack);
 
-    // 
-    // Disabled temporarily, because of external dependency
-    // 
-    // import lodash from 'lodash';
-    // 
+    if ( haystack.length < 1 ) { 
+        return undefined; }
 
-	// needle = float( needle );
-	// haystack = kp.hench.array.valid( haystack );
+    let closest = haystack[0];
+    let smallest_diff = Math.abs(float(closest) - needle);
 
-	// const abs_objs = haystack.map(( itm ) => ( {val:itm,abs:Math.abs(float(itm)-needle)} ));
-	// const nearest = lodash.sortBy( abs_objs, ['abs'] );
+    for (const itm of haystack) {
+        const diff = Math.abs(float(itm) - needle);
+        if (diff < smallest_diff) {
+            smallest_diff = diff;
+            closest = itm;
+        }
+    }
 
-	// const nearest_number = nearest[0]?.val;
+    return closest;
 
-	// return nearest_number;
 }
 
 /**
@@ -83,21 +95,22 @@ export function nearest_in_array({ needle, haystack }) {
  */
 export function nearest_next({ needle, haystack }) {
 
-	needle = float( needle );
-	haystack = kp.hench.array.valid( haystack );
-	haystack = haystack.sort((l,r) => kp.hench.compare_to_sort(l,r));
+    needle = float(needle);
+    haystack = kp.hench.array.valid(haystack);
+    haystack = haystack.sort((l, r) => kp.hench.compare_to_sort(l, r));
 
-	const abs_objs = haystack.map(( itm ) => ( {val:itm,abs:Math.abs(float(itm)-needle)} ));
-	const nearest = nearest_in_array({ needle, haystack });
-	if ( nearest > needle ) {
-		return nearest; }
 
-	let idx = haystack.indexOf( nearest );
-	idx = Math.min( ( haystack.length - 1 ), Math.max( 0, ( idx + 1 ) ) );
+    const nearest = nearest_in_array({ needle, haystack });
+    if (nearest > needle) {
+        return nearest;
+    }
 
-	const nearest_next = haystack[idx];
+    let idx = haystack.indexOf(nearest);
+    idx = Math.min((haystack.length - 1), Math.max(0, (idx + 1)));
 
-	return nearest_next;
+    const nearest_next = haystack[idx];
+
+    return nearest_next;
 
 }
 
@@ -107,20 +120,21 @@ export function nearest_next({ needle, haystack }) {
  */
 export function nearest_prev({ needle, haystack }) {
 
-	needle = float( needle );
-	haystack = kp.hench.array.valid( haystack );
-	haystack = haystack.sort((l,r) => kp.hench.compare_to_sort(l,r));
+    needle = float(needle);
+    haystack = kp.hench.array.valid(haystack);
+    haystack = haystack.sort((l, r) => kp.hench.compare_to_sort(l, r));
 
-	const abs_objs = haystack.map(( itm ) => ( {val:itm,abs:Math.abs(float(itm)-needle)} ));
-	const nearest = nearest_in_array({ needle, haystack });
-	if ( nearest < needle ) {
-		return nearest; }
 
-	let idx = haystack.indexOf( nearest );
-	idx = Math.min( ( haystack.length - 1 ), Math.max( 0, ( idx - 1 ) ) );
+    const nearest = nearest_in_array({ needle, haystack });
+    if (nearest < needle) {
+        return nearest;
+    }
 
-	const nearest_prev = haystack[idx];
+    let idx = haystack.indexOf(nearest);
+    idx = Math.min((haystack.length - 1), Math.max(0, (idx - 1)));
 
-	return nearest_prev;
+    const nearest_prev = haystack[idx];
+
+    return nearest_prev;
 
 }
