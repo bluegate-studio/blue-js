b511a21a572f07d5b6b08b453d82d7aff09c4af1
Bluegate Studio <144131259+bluegate-studio@users.noreply.github.com>
2026-02-18 23:47:40 +0300

    debug: disable shell module temporarily

--- src/_.js
+++ src/_.js
@@ -1,7 +1,3 @@
 export * as hench from './hench/_.js';
 export * as linguist from './linguist/_.js';
 export * as console from './console/_.js';
-
-const is_node = typeof process !== 'undefined' && process.versions?.node;
-const _shell = is_node ? await import('./shell/_.js') : {};
-export { _shell as shell };
