da3a8682ff653c7f1f3f2448b3d89eee3eab73cd
Bluegate Studio <144131259+bluegate-studio@users.noreply.github.com>
2026-02-19 00:16:11 +0300

    remove shell module from exports; drop browser entry point experiment

--- src/_.js
+++ src/_.js
@@ -2,4 +2,4 @@ export * as hench from './hench/_.js';
 export * as linguist from './linguist/_.js';
 export * as console from './console/_.js';
 
-export * as shell from './shell/_.js';
+// export * as shell from './shell/_.js';
